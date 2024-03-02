a5782507efed57cf4465aaa23084fef68fd43a2f
bander <bannadr1@hotmail.com>
2024-03-02 15:47:20 +0300

    udpate image tage

--- build_and_push_amd.sh
+++ build_and_push_amd.sh
@@ -1,3 +1,3 @@
 DOCKER_BUILDKIT=1 docker build --platform linux/amd64 . -t nodejs-edge-rdkafka-v2-amd64
-docker tag nodejs-edge-rdkafka-v2-amd64:latest bandersaeed94/la79y:edge
-docker push bandersaeed94/la79y:edge
+docker tag nodejs-edge-rdkafka-v2-amd64:latest bandersaeed94/la79y:edge-v3
+docker push bandersaeed94/la79y:edge-v3
