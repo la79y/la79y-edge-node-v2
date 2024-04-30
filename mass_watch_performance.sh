9a3196e5137ce5fc7ce8d86ce12aa2e7cf5935b6
Sana Garah <sgarah-e@jahez.net>
2024-04-30 21:46:23 +0300

    added static session id for testing

--- mass_watch_performance.sh
+++ mass_watch_performance.sh
@@ -1,19 +1,23 @@
-for i in {1..51}
+# Create directories for samples and ffprobe outputs if they do not exist
+mkdir -p samples
+mkdir -p ffprobe_outputs
+
+for i in {1..5}
 do
-    # Record a segment of the stream
-    ffmpeg -i 'srt://164.90.241.38:10081?streamid=#!::u=bander1234,r=livestream4,m=request,t=stream,s=Session_ID' -t 10 -c copy sample${i}.ts &
+    # Record a segment of the stream and store it in the samples folder
+    ffmpeg -i 'srt://127.0.0.1:1235?streamid=#!::u=admin,r=test,s=12345678-1234-1234-1234-123456789abc,m=request,t=stream&passphrase=edge_pass1234&pbkeylen=16' -t 10 -c copy samples/sample${i}.ts &
 done
 
 # Wait for all ffmpeg instances to finish
 wait
 
-for i in {1..51}
+for i in {1..5}
 do
-    # Play recorded segment (Optional, based on your scenario)
-    ffplay sample${i}.ts 2>&1 | tee ffplay_output_$(date +%s).log &
+    # Optionally, play recorded segment and log output (log files will be in the current directory)
+    ffplay samples/sample${i}.ts 2>&1 | tee ffplay_output_$(date +%s).log &
 
-    # Analyze the recorded segment with ffprobe
-    ffprobe -v error -show_format -show_streams sample${i}.ts > ffprobe_output_${i}.log
+    # Analyze the recorded segment with ffprobe and save the log in the ffprobe_outputs folder
+    ffprobe -v error -show_format -show_streams samples/sample${i}.ts > ffprobe_outputs/ffprobe_output_${i}.log
 done
 
 # Wait for all ffplay instances to finish, if they were used
