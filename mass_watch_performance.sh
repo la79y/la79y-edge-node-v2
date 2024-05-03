2432f6dfc7bf8ffdaaf273bd21d31ec195e227b3
Sana Garah <sgarah-e@jahez.net>
2024-05-03 20:27:34 +0300

    feat: performance measurments for mass watch

--- mass_watch_performance.sh
+++ mass_watch_performance.sh
@@ -2,16 +2,16 @@
 mkdir -p samples
 mkdir -p ffprobe_outputs
 
-for i in {1..5}
+for i in {1..49}
 do
     # Record a segment of the stream and store it in the samples folder
-    ffmpeg -i 'srt://127.0.0.1:1235?streamid=#!::u=admin,r=test,s=12345678-1234-1234-1234-123456789abc,m=request,t=stream&passphrase=edge_pass1234&pbkeylen=16' -t 10 -c copy samples/sample${i}.ts &
-done
+    ffmpeg -i `srt://161.35.240.212:10081?streamid=#!::u=admin${i},r=252ad23f-1eb2-476e-931a-ca9155ec00ca,s=12345678-1234-1234-1234-123456789abc,m=request,t=stream&passphrase=edge_pass1234&pbkeylen=16` -t 10 -c copy samples/sample${i}.ts &
+done 
 
 # Wait for all ffmpeg instances to finish
 wait
 
-for i in {1..5}
+for i in {1..49}
 do
     # Optionally, play recorded segment and log output (log files will be in the current directory)
     ffplay samples/sample${i}.ts 2>&1 | tee ffplay_output_$(date +%s).log &
--- performance_comparison.py
+++ performance_comparison.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import os
 import re
@@ -20,10 +22,9 @@ resolution_pattern = re.compile(r'width=(\d+)\s+height=(\d+)')
 frame_rate_pattern = re.compile(r'r_frame_rate=(\d+)/1')
 sample_rate_pattern = re.compile(r'sample_rate=(\d+)')
 video_start_time_pattern = re.compile(r'start_time=(\d+\.\d+)')
-video_duration_pattern = re.compile(r'duration=(\d+\.\d+)\s+bit_rate')
-audio_duration_pattern = re.compile(r'duration=(\d+\.\d+)')
-file_size_pattern = re.compile(r'size=(\d+)')
 bit_rate_pattern = re.compile(r'bit_rate=(\d+)')
+color_space_pattern = re.compile(r'color_space=([\w-]+)')
+color_range_pattern = re.compile(r'color_range=([\w-]+)')
 
 # Iterate over each file in the directory
 for filename in os.listdir(directory):
@@ -40,21 +41,18 @@ for filename in os.listdir(directory):
             frame_rate_match = frame_rate_pattern.search(content)
             sample_rate_match = sample_rate_pattern.search(content)
             video_start_time_match = video_start_time_pattern.search(content)
-            video_duration_match = video_duration_pattern.search(content, re.MULTILINE)
-            audio_duration_matches = audio_duration_pattern.findall(content)
-            file_size_match = file_size_pattern.search(content)
-            bit_rate_match = bit_rate_pattern.search(content)
+            bit_rate_matches = bit_rate_pattern.findall(content)
+            color_space_match = color_space_pattern.search(content)
+            color_range_match = color_range_pattern.search(content)
             
             # Convert and calculate necessary values
             resolution = f"{resolution_match.group(1)}x{resolution_match.group(2)}" if resolution_match else "N/A"
             frame_rate = int(frame_rate_match.group(1)) if frame_rate_match else "N/A"
             sample_rate = int(sample_rate_match.group(1)) if sample_rate_match else "N/A"
             video_start_time = float(video_start_time_match.group(1)) if video_start_time_match else "N/A"
-            video_duration = float(video_duration_match.group(1)) if video_duration_match else "N/A"
-            audio_duration_str = audio_duration_matches[-1] if audio_duration_matches else "N/A"
-            audio_duration = float(audio_duration_str) if is_float(audio_duration_str) else "N/A"
-            file_size_mb = int(file_size_match.group(1)) / 1024**2 if file_size_match else "N/A"
-            bit_rate_mbps = int(bit_rate_match.group(1)) / 10**6 if bit_rate_match else "N/A"
+            bit_rate_mbps = int(bit_rate_matches[1]) if len(bit_rate_matches) > 1 else (int(bit_rate_matches[0]) if bit_rate_matches else "N/A")
+            color_space = color_space_match.group(1) if color_space_match else "N/A"
+            color_range = color_range_match.group(1) if color_range_match else "N/A"
             
             # Append data to the list
             data.append({
@@ -63,10 +61,9 @@ for filename in os.listdir(directory):
                 "Frame Rate (fps)": frame_rate,
                 "Sample Rate (Hz)": sample_rate,
                 "Video Start Time (s)": video_start_time,
-                "Video Duration (s)": video_duration,
-                "Audio Duration (s)": audio_duration,
-                "File Size (MB)": file_size_mb,
-                "Bit Rate (Mbps)": bit_rate_mbps
+                "Bit Rate (bps)": bit_rate_mbps,
+                "Color Space": color_space,
+                'color_range': color_range
             })
 
 # Create DataFrame from the collected data
